a9e5824be7ecb36713bcdbc830927a628254217c
OTMANE <davenamir3@gmail.com>
2025-01-07 20:15:21 +0100

    fsd

--- console.py
+++ console.py
@@ -143,6 +143,7 @@ class HBNBCommand(cmd.Cmd):
             new_instance.save()
 
 
+
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
