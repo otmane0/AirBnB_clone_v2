e48c5787b8f8f5df1e810291d6f92d3f1482395a
OTMANE <davenamir3@gmail.com>
2025-02-18 21:19:28 +0100

    fix config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
+# make ur web server works
+dpkg -l | grep nginx > /dev/null 2>&1 || (sudo apt -y update && sudo apt -y upgrade && sudo apt -y install nginx)
+ls /data/web_static/releases/test/ > /dev/null 2>&1 || sudo mkdir -p /data/web_static/releases/test/
+ls /data/web_static/shared/ > /dev/null 2>&1 || sudo mkdir -p /data/web_static/shared/
 
-dpkg -l | grep nginx  > /dev/null 2>&1 || (sudo apt -y update && sudo apt -y upgrade && sudo apt -y install nginx)
-
-declare -a dirs=(
-  "/data"
-  "/data/web_static"
-  "/data/web_static/releases"
-  "/data/web_static/shared"
-  "/data/web_static/releases/test"
-)
-
-for dir in "${dirs[@]}"; do
-  [ ! -d "$dir" ] && sudo mkdir -p "$dir" > /dev/null 2>&1
-done
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
+sudo service nginx restart
