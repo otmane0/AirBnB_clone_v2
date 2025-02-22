0942a7fc6e1bcc76b07a516233d9f25d02205e45
OTMANE <davenamir3@gmail.com>
2025-02-22 15:20:49 +0100

    chick again

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,8 +14,10 @@ def do_pack():
         Archive path if successful, None otherwise.
     """
     try:
+
         # Create the 'versions' directory if it doesn't exist
         if not os.path.exists("versions"):
+            print("Creating 'versions' directory...")
             os.system("mkdir -p versions")
 
         # Generate the archive name using the current timestamp
@@ -29,10 +31,13 @@ def do_pack():
         result = os.system("tar -cvzf {} -C web_static .".format(archive_path))
 
         # Check if the archive was created successfully
-
         if result == 0:  # tar command returns 0 on success
             return archive_path
         else:
             return None
     except Exception as e:
         return None
+
+# Run the do_pack function
+if __name__ == "__main__":
+    do_pack()
